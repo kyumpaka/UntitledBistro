b8dce45a4606d49bd00fff18979f25b4fe64570c
SeJongDeveloper <51711799+sejongdeveloper@users.noreply.github.com>
2019-09-17 16:34:38 +0900

    타일즈 적용

--- UB/src/main/java/com/bit/UntitledBistro/controller/HomeController.java
+++ UB/src/main/java/com/bit/UntitledBistro/controller/HomeController.java
@@ -1,28 +1,21 @@
 package com.bit.UntitledBistro.controller;
 
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
-	public String home(Locale locale) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		return "baseLayout"; 
+	@RequestMapping(value = "/")
+	public String test() {
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		return "baseLayout";
 	}
 	
-	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
-	public String test() {
-		logger.info("테스트 컨트롤러입니다");
-		return "baseLayout";
+	@RequestMapping(value = "/b")
+	public String b() {
+		System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+		return "jaego/test";
 	}
 	
 }
